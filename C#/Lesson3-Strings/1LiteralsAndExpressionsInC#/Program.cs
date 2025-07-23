bd08ba2aa8c13e1fe43f20d7642ffda9cad51970
André Macáu <andremacau1983@gmail.com>
2025-07-23 13:26:09 -0300

    Removed

--- C#/Lesson3-Strings/1LiteralsAndExpressionsInC#/Program.cs
+++ C#/Lesson3-Strings/1LiteralsAndExpressionsInC#/Program.cs
@@ -8,17 +8,6 @@ namespace LiteralsAndExpressionsInCSharp
         {
             var str = new Strings();
             Console.WriteLine("str1:\n" + str.ToString());
-            // Console.WriteLine("\nstr2:\n" + str2);
-            // Console.WriteLine("\nstr3:\n" + str3);
-            // Console.WriteLine("\nstr4:\n" + str4);
-            // Console.WriteLine("\nstr5:\n" + str5);
-            // Console.WriteLine("\nstr6:\n" + str6);
-            // Console.WriteLine("\nstr7:\n" + str7);
-            // Console.WriteLine("\nstr8:\n" + str8);
-            // Console.WriteLine("\nstr9:\n" + str9);
-            // Console.WriteLine("\nstr10:\n" + str10);
-            // Console.WriteLine("\nstr11:\n" + str11);
-
         }
     }
 }
